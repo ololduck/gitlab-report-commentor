86e31dcb36b427156eb4d3a6295350c1924015ab
Paul O <contact@paulollivier.fr>
2023-03-30 12:41:56 +0200

    initial commit

---
+++ src/input_formats/sast.rs
@@ -0,0 +1,100 @@
+use askama::Template;
+use chrono::{DateTime, NaiveDateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+use super::{ReportFormatHandler, Severity};
+
+const DEFAULT_NAME: &str = "<unnamed>";
+
+pub struct SastHandler;
+
+#[derive(Template)]
+#[template(path = "sast/vulnerability_report.md.j2")]
+struct VulnerabilityReportTemplate<'a> {
+    vulnerabilities: &'a Vec<Vulnerability>,
+    scan: &'a Scan,
+}
+
+impl ReportFormatHandler for SastHandler {
+    type ReportFormat = SastReport;
+    fn parse_to_struct(s: &str) -> Self::ReportFormat {
+        serde_json::from_str(s).expect("Could not parse given string as proper SAST report")
+    }
+
+    fn render_to_markdown(doc: &Self::ReportFormat) -> String {
+        let report = VulnerabilityReportTemplate {
+            vulnerabilities: &doc.vulnerabilities,
+            scan: &doc.scan,
+        };
+        report.render().unwrap()
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct SastReport {
+    version: String,
+    vulnerabilities: Vec<Vulnerability>,
+    dependency_files: Vec<()>,
+    scan: Scan,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct Vulnerability {
+    id: String,
+    category: String,
+    #[serde(default)]
+    name: String,
+    message: String,
+    description: String,
+    cve: String,
+    severity: Severity,
+    scanner: VulnerabilityScanner,
+    location: VulnerabilityLocation,
+    identifiers: Vec<VulnerabilityIdentifier>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct VulnerabilityScanner {
+    id: String,
+    name: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct VulnerabilityLocation {
+    file: String,
+    start_line: u32,
+    end_line: Option<u32>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct VulnerabilityIdentifier {
+    #[serde(rename = "type")]
+    vulnerability_type: String,
+    name: String,
+    value: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct Scan {
+    analyzer: Software,
+    scanner: Software,
+    #[serde(rename = "type")]
+    scan_type: String,
+    start_time: NaiveDateTime,
+    end_time: NaiveDateTime,
+    status: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct Software {
+    id: String,
+    name: String,
+    url: String,
+    vendor: SoftwareVendor,
+    version: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct SoftwareVendor {
+    name: String,
+}
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use std::io;
+use std::io::Read;
+
+mod input_formats;
+
+use crate::input_formats::sast::SastHandler;
+use crate::input_formats::Handlers;
+use crate::input_formats::ReportFormatHandler;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(short = 't', long = "type", value_enum)]
+    handler: Handlers,
+    #[arg(short, long)]
+    dry_run: bool,
+}
+
+fn render_to_comment(handler: Handlers, raw_doc: &str) -> String {
+    match handler {
+        Handlers::Sast => SastHandler::render_to_markdown(&SastHandler::parse_to_struct(&raw_doc)),
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut raw_doc = String::new();
+    let raw_doc_len = io::stdin()
+        .read_to_string(&mut raw_doc)
+        .expect("Could not read from stdin");
+    if raw_doc_len == 0 {
+        panic!("Read a string of length 0 from stdin!");
+    }
+    let comment = render_to_comment(args.handler, &raw_doc);
+    println!("{}", comment);
+    // then upload as gitlab MR comment if we can find the appropriate envvars
+}
