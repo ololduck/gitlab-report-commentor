fa00249e8e942209cb2a45a3000ca4dca7272bb7
Paul Ollivier <contact@paulollivier.fr>
2023-03-31 20:36:24 +0200

    Switch to handlebars, add poster, add readme.

--- src/poster.rs
+++ src/poster.rs
@@ -4,21 +4,21 @@ use std::env;
 use reqwest::blocking::Client;
 
 pub fn post_to_merge_request(comment: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let base_url = env::var("GITLAB_URL")?;
-    let project_id = env::var("PROJECT_ID")?;
-    if let Ok(merge_request_id) = env::var("MERGE_REQUEST_ID") {
-        let client = Client::new();
-        let _resp = client
-            .post(format!(
-                "{base_url}/api/v4/projects/{project_id}/merge_requests/{merge_request_id}/notes"
-            ))
-            .header("PRIVATE-TOKEN", env::var("GITLAB_USER_TOKEN")?)
-            .json(&{
-                let mut json = HashMap::new();
-                json.insert("body", comment.to_string());
-                json
-            })
-            .send()?;
-    }
+    let base_url = env::var("CI_API_V4_URL").expect("Could not find CI_API_V4_URL in the environment");
+    let project_id = env::var("CI_PROJECT_ID").expect("Could not find CI_PROJECT_ID in the environment");
+    let merge_request_id = env::var("CI_MERGE_REQUEST_IID").expect("Could not find envvar CI_MERGE_REQUEST_IID, am I running in a merge request pipeline?");
+    let client = Client::new();
+    let _resp = client
+        .post(format!(
+            "{base_url}/projects/{project_id}/merge_requests/{merge_request_id}/notes"
+        ))
+        .header("PRIVATE-TOKEN", env::var("GITLAB_USER_TOKEN")?)
+        .json(&{
+            let mut json = HashMap::new();
+            json.insert("body", comment.to_string());
+            json
+        })
+        .send()?;
+
     Ok(())
 }
