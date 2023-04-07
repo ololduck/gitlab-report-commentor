324c6daf5469af59846e27fbc9a1c7b6702ef34a
Paul Ollivier <contact@paulollivier.fr>
2023-04-07 20:59:20 +0200

    an attempt to be more human-friendly in our error management

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use clap::Parser;
 use std::io;
 use std::io::Read;
+use std::process::exit;
+use exitfailure::ExitFailure;
+use human_panic::{setup_panic};
+use pretty_env_logger;
+use pretty_env_logger::env_logger::Builder;
+use log::{debug, error, info, warn};
 
 mod input_formats;
 mod poster;
@@ -9,34 +15,49 @@ use crate::input_formats::sast::SastHandler;
 use crate::input_formats::Handlers;
 use crate::input_formats::ReportFormatHandler;
 
+const EMPTY_FILE_ERROR: i32 = 1;
+const BAD_CONFIG: i32 = 2;
+
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
     #[arg(short = 't', long = "type", value_enum)]
     handler: Handlers,
     #[arg(
-        short,
-        long,
-        help = "don't try to actually post the message, just parse the report."
+    short,
+    long,
+    help = "don't try to actually post the message, just parse the report."
     )]
     dry_run: bool,
 }
 
 /// Takes a given handler, matches it with i
 fn render_to_comment(handler: Handlers, raw_doc: &str) -> String {
-    match handler {
+    let s = match handler {
         Handlers::Sast => SastHandler::render_to_markdown(&SastHandler::parse_to_struct(raw_doc)),
-    }
+    };
+    info!("Successfully parsed input");
+    debug!("Rendered template:\n{}", &s);
+    s
 }
 
-fn main() {
+
+fn main() -> Result<(), ExitFailure> {
+    setup_panic!();
+    Builder::from_env("LOG_LEVEL").format_level(true).format_module_path(false).format_timestamp(None).init();
     let args = Args::parse();
     let mut raw_doc = String::new();
-    let raw_doc_len = io::stdin()
-        .read_to_string(&mut raw_doc)
-        .expect("Could not read from stdin");
+    let raw_doc_len = match io::stdin()
+        .read_to_string(&mut raw_doc) {
+        Ok(size) => size,
+        Err(e) => {
+            error!("Could not read from stdin: {}", e);
+            exit(EMPTY_FILE_ERROR);
+        }
+    };
     if raw_doc_len == 0 {
-        panic!("Read a string of length 0 from stdin!");
+        error!("Got an empty file on stdin.");
+        exit(EMPTY_FILE_ERROR);
     }
     let comment = render_to_comment(args.handler, &raw_doc);
     println!("{}", comment);
@@ -44,5 +65,8 @@ fn main() {
     if !args.dry_run {
         poster::post_to_merge_request(&comment)
             .expect("Could not post. Maybe you are missing some envvars?");
+    } else {
+        warn!("As --dry-run has been specified, I will stop here.");
     }
+    Ok(())
 }
--- src/poster.rs
+++ src/poster.rs
@@ -1,22 +1,35 @@
 use std::collections::HashMap;
 use std::env;
+use std::process::exit;
+use log::error;
 
 use reqwest::blocking::Client;
+use crate::BAD_CONFIG;
 
-pub fn post_to_merge_request(comment: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let base_url =
-        env::var("CI_API_V4_URL").expect("Could not find CI_API_V4_URL in the environment");
-    let project_id =
-        env::var("CI_PROJECT_ID").expect("Could not find CI_PROJECT_ID in the environment");
-    let merge_request_id = env::var("CI_MERGE_REQUEST_IID").expect(
-        "Could not find envvar CI_MERGE_REQUEST_IID, am I running in a merge request pipeline?",
-    );
+pub fn post_to_merge_request(comment: &str) -> Result<(), failure::Error> {
+    let Ok(base_url) = env::var("CI_API_V4_URL") else {
+        error!("Could not find CI_API_V4_URL in the environment, which is a default GitLab-CI variable. Am I running on GitLab-CI?");
+        exit(BAD_CONFIG);
+    };
+    let Ok(project_id) =
+        env::var("CI_PROJECT_ID") else {
+        error!("Could not find CI_PROJECT_ID in the environment, which is a default GitLab-CI variable. Am I running on GitLab-CI?");
+        exit(BAD_CONFIG);
+    };
+    let Ok(merge_request_id) = env::var("CI_MERGE_REQUEST_IID")else {
+        error!("Could not find CI_MERGE_REQUEST_IID in the environment, am I running in a merge request pipeline?");
+        exit(BAD_CONFIG);
+    };
+    let Ok(gitlab_token) = env::var("GITLAB_USER_TOKEN") else {
+        error!("Could not find GITLAB_USER_TOKEN in the environment. I need it to send what I have to say.");
+        exit(BAD_CONFIG);
+    };
     let client = Client::new();
     let _resp = client
         .post(format!(
             "{base_url}/projects/{project_id}/merge_requests/{merge_request_id}/notes"
         ))
-        .header("PRIVATE-TOKEN", env::var("GITLAB_USER_TOKEN")?)
+        .header("PRIVATE-TOKEN", gitlab_token)
         .json(&{
             let mut json = HashMap::new();
             json.insert("body", comment.to_string());
